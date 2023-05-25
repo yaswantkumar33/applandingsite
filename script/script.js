eb3b2e6afcb6df8c22c7a8b2580c26fced34c113
Yaswant Kumar <yaswantkumar1512@gmail.com>
2023-05-25 14:39:54 +0530

    second commit

--- script/script.js
+++ script/script.js
@@ -52,6 +52,24 @@ $(window).on('load scroll', function () {
             }
         }
     });
+    $('.review-slider').owlCarousel({
+        loop:true,
+        center:true,
+        autoplay:true,
+        nav:false,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            750:{
+                items:2
+            },
+            1200:{
+                items:3
+            }
+        }
+    });
 
 
 
